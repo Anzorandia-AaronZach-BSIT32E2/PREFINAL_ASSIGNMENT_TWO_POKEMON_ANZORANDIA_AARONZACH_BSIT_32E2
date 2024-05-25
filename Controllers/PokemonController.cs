09ebb12365b3355d3541356280bb5e228e70609c
Anzorandia_Aaron Zach_BSIT32E2 <zanzorandia@gmail.com>
2024-05-26 01:50:35 +0800

    Add project files.

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,4 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PREFINAL_ASSIGNMENT_TWO_POKEMON_ANZORANDIA_AARONZACH_BSIT_32E2.Models;
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_ANZORANDIA_AARONZACH_BSIT_32E2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
